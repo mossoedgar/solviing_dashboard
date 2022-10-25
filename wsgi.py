565a435d52971029b19343191201576f4d601c1a
Edgar Mosso <36419792+mossoedgar@users.noreply.github.com>
2022-10-24 22:58:49 -0500

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,5 @@
 """Application entry point."""
-from plotlyflask_tutorial import init_app
-
-
-app = init_app()
-
+from app import app
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
